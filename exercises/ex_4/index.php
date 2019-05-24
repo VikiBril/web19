8f366948c543831d4b3d324a1bce037848e52ae1
viki bril <bril2010@gmail.com>
2019-05-24 05:09:26 +0300

    ex_4

---
+++ exercises/ex_4/index.php
@@ -0,0 +1,22 @@
+form action="http://html5-book.co.il/teach/forms/get_form2.php" method="get">
+<label>What is your full name? <input name="fullName">
+<input type="text" name="myName" value=""> </label>
+
+<label>
+User Name:
+<input type="text" name="username" value="">
+</label>
+
+<label>Your name is:<input name="fullName"></label>
+<label>Which Disney Princess do you prefer? <input name="pass"></label>
+<label>What is your favorite color out of the following?<input name="color"></label>
+<label>I live in a?  <input name="mail"></label>
+<label> My favorite weekend activity is? <input name="website"></label>
+<label>he animal who will lead my (inevitable) royal parade is: <input name="phone"></label>
+<label>The most important quality I look for in A prince is:<input name="coffee"></label>
+<label>Which of these outfit elements are you most likely to wear? <input name="temper"></label>
+<label> My favorite place to burst into song is:<input name="address"></label>
+<label>My favorite hair style is<input name="gender"></label>
+<label>My age is:<input name="fullName"></label>
+
+</form>
